72ad20e99f7581929d17c4271b3c73d24ee21e77
kcandeloni <kcandeloni@gmail.com>
2022-09-30 00:56:15 -0300

    conexão com o banco + rota categories

---
+++ src/database/database.js
@@ -0,0 +1,14 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const connection = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '123456',
+    database: 'boardcamp',
+  //connectionString: process.env.DATABASE_URL,
+});
+
+export default connection;
