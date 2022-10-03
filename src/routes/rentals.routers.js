84034d85f1b7437558bb1b67a2bd4d86abb0d50b
kcandeloni <kcandeloni@gmail.com>
2022-10-03 16:11:27 -0300

    validate new rentals

--- src/routes/rentals.routers.js
+++ src/routes/rentals.routers.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import getRentals from '../controllers/rentals_controllers/getRentals.controllers.js';
 import createRentals from '../controllers/rentals_controllers/createRentals.controllers.js';
+import validateRentals from '../middlewares/validateRentals.middlewares.js';
 
 const rentalsRouter = express.Router();
 rentalsRouter.get('/rentals', getRentals);
-rentalsRouter.post('/rentals', createRentals);
+rentalsRouter.post('/rentals', validateRentals, createRentals);
 //rentalsRouter.get('/rentals/:id', getCustomersId);
 //rentalsRouter.put('/rentals/:id', validateCustomer, updateCustomers);
 
