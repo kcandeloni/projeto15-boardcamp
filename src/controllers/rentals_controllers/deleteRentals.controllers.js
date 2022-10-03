0b6b15a8e9e60922c4cc8da4be3ad6946923e194
kcandeloni <kcandeloni@gmail.com>
2022-10-03 17:59:30 -0300

    delete rentals, só

---
+++ src/controllers/rentals_controllers/deleteRentals.controllers.js
@@ -0,0 +1,26 @@
+import connection from '../../database/database.js';
+
+async function deleteRentals (req, res) {
+    const id = Number.isInteger(parseInt(req.params.id)) ? parseInt(req.params.id) : 0;
+    try {
+        const rentals = await connection.query(
+            `SELECT * FROM rentals WHERE id = $1;`,[id]);
+        if(rentals?.rows?.length < 1){    
+            return res.sendStatus(404);
+        }
+        if(rentals?.rows[0]?.returnDate !== null){
+            return res.sendStatus(400);
+        }
+
+        await connection.query(
+            `DELETE FROM rentals WHERE id = $1;`, [id]);
+
+        return res.sendStatus(200);
+       
+        } catch (err) {
+            console.error(err);
+            res.sendStatus(500);
+        }
+}
+
+export default deleteRentals;
